0af6adb293b294fdeab450966997daef3854216b
atef <atef.benmohamed@smarttouchtunisie.com>
2017-01-26 05:43:50 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+agency
+======
+
+A Symfony project created on January 26, 2017, 3:13 am.<br/>
+
+[Template frontend link](http://templates.theemon.com/?theme=letsdrive-html)
---
+++ src/Agency/FrontendBundle/Controller/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Agency\FrontendBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class HomeController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('FrontendBundle:Home:index.html.twig');
+    }
+}
